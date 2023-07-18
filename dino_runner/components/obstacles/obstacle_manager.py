dbd7a04a8fa9bff8027dfbc9864b100554920199
DaviJuniorjr <113080797+DaviJuniorjr@users.noreply.github.com>
2023-07-18 19:59:29 -0300

    Atividade Cactus e Birds

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -2,15 +2,21 @@ import pygame
 
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import LARGE_CACTUS
+from dino_runner.utils.constants import BIRD
 
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
+        self.spawn_distance = 600
 
+    
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            self.obstacles.append(Cactus(SMALL_CACTUS))          
+            self.obstacles.append(Cactus(LARGE_CACTUS))  
+            self.obstacles.append(Cactus(BIRD))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
